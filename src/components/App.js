933cfec018202eba1cc38d1d252930dbd61af2b2
M. Rehan <m.rehan1016@gmail.com>
2021-08-16 23:54:19 +0530

    added contact lists

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,25 @@ import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      "name": "Rehan",
+      "email": "m.rehan1016@gmail.com",
+    },
+    {
+      id: "2",
+      "name": "Ajay",
+      "email": "ajay12@gmail.com",
+    },
+  ];
   return (
-    <div>
+    <div className="ui container">
       <Header/>
+      <AddContact />
+      <ContactList />
     </div>
   );
-}
+};
 
 export default App;
